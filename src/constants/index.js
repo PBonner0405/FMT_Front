1ecc6f02a1b94ab33f6b7eb1345db57207f71bd9
Avantica777 <avantica777@gmail.com>
2019-05-03 17:02:35 +0300

    clear project

--- src/constants/index.js
+++
@@ -1,6 +0,0 @@
-export const APP_IS_READY = 'APP_IS_READY';
-export const APP_IS_NOT_READY = 'APP_IS_NOT_READY';
-
-export const SIGN_IN = 'SIGN_IN';
-export const SIGN_OUT = 'SIGN_OUT';
-export const SIGN_ON = 'SIGN_ON';
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './user';
-import stockReducer from './stock';
-const reducer = combineReducers({
-  user: userReducer,
-  stocks: stockReducer
-});
-
-export default reducer;
